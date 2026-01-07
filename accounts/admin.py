cffb5a3b461e55a8b6c3741b5af0b2520132ff4c
Mauli Prakash Karande <164602755+MauliKarande@users.noreply.github.com>
2026-01-07 17:58:54 +0530

    Registered User and Department models in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Department
 
-# Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'department',
+        'is_active',
+        'is_staff',
+    )
+
+    list_filter = ('role', 'department', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Company Details', {
+            'fields': ('role', 'department'),
+        }),
+    )
